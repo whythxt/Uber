b20507856e00da095bb2cd55611758cacab364fd
Yurii <yuraznakharchuk@gmail.com>
2023-01-19 16:50:47 +0200

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Yurii on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
